6fe697b8035ac8c6e7822f6ffc48f318cbf375f0
Jagteshver Singh <85994817+JS-CodeGamer@users.noreply.github.com>
2023-03-22 22:22:37 +0530

    Revert "Revert "Vaibhav""

--- UserAPI/utils.py
+++ UserAPI/utils.py
@@ -3,14 +3,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ifscApi.getDetails import FetchData
 import requests
-# Assign IFSC code
-ifsc = 'KKBK0005652'
+import jwt
+from django.conf import settings
+from django.core import exceptions
+# # Assign IFSC code
+# ifsc = 'KKBK0005652'
   
-# Parse the ifsc code
-data = FetchData().getdata(ifsc)
+# # Parse the ifsc code
+# data = FetchData().getdata(ifsc)
   
-# Display details
-print(data)
+# # Display details
+# print(data)
 
 def bank_details(ifsc):
     try:
@@ -18,3 +21,21 @@ def bank_details(ifsc):
         return res.json()
     except Exception as e:
         return Response({'error':e},status=200)
+    
+def decode_token(request):
+    auth_header = request.headers.get('Authorization')
+    if not auth_header:
+        raise Exception('Authorization header is missing')
+    _, token = auth_header.split()
+    try:
+        decoded_token = jwt.decode(token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHM])
+        return decoded_token
+    except jwt.InvalidSignatureError:    
+        raise Exception('Token Invalid')
+    except jwt.ExpiredSignatureError:
+        raise Exception('Token Expired')
+    except IndexError:
+        raise Exception('Token prefix missing')
+    except Exception as e:
+        raise Exception(str(e))
+    
